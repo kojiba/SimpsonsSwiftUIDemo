746641ded200a0bcf87422d86e23e1b0b0425dba
kojiba <kojiba@ro.ru>
2020-12-02 10:42:30 +0200

    Initial Commit

---
+++ SimpsonsSwiftUIDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SimpsonsSwiftUIDemo
+//
+//  Created by kojiba on 02.12.2020.
+//  Copyright © 2020 kojiba. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
